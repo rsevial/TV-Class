cc6b2bb7b6d573ccfea98ca2cc963042e439723e
rsevial <rebekahsevial@gmail.com>
2023-05-15 15:45:37 +0800

    Continue Pseudocode

--- TV.py
+++ TV.py
@@ -1,4 +1,12 @@
 # Programmed by: Rebekah Joy E. Sevial
 # Creating Class named TV
 
-# Creating the Class named TV
+# Creating the class named TV
+# Define constructor of the TV
+# Define function to turn on the TV
+# Define function to turn off the TV
+# Define function to return the channel
+# Define function to check if the channel is existing
+
+
+
--- TestTV.py
+++ TestTV.py
@@ -1,4 +1,4 @@
 # Programmed by: Rebekah Joy E. Sevial
 # Test Driver Program
 
-# Creating the class named TestTV
+
