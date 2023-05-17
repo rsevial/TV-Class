5509747225f95f8e89c498bd7ace45fbe1cf0aa9
rsevial <rebekahsevial@gmail.com>
2023-05-17 15:35:09 +0800

    Edit define function and assigning the values of each attributes of TV1 and TV2

--- TestTV.py
+++ TestTV.py
@@ -4,10 +4,18 @@
 # Import the TV class of TV file
 from TV import TV
 # Define function for testing the two TV's
-def TestTV()
-    tv1 = ()
-    tv2 = ()
+def TestTV():
+    tv1 = TV()
+    tv2 = TV()
 # Assigning the values of each attributes of TV1 and TV2
+    # TV1
+    tv1.turnOn()
+    tv1.setChannel(30)
+    tv1.setVolume(3)
+    # TV2
+    tv2.turnOn()
+    tv2.setChannel(3)
+    tv2.setVolume(2)
 # Print the TV1's and TV2's channel and volume
 # Call the main function
 
