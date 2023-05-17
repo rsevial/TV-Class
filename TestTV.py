1db81b90d819e308fcb00cd570f26d381a93d837
rsevial <rebekahsevial@gmail.com>
2023-05-17 15:13:13 +0800

    Define function for testing the two TV's

--- TestTV.py
+++ TestTV.py
@@ -4,6 +4,9 @@
 # Import the TV class of TV file
 from TV import TV
 # Define function for testing the two TV's
+def TestTV()
+    tv1 = ()
+    tv2 = ()
 # Assigning the values of each attributes of TV1 and TV2
 # Print the TV1's and TV2's channel and volume
 # Call the main function
