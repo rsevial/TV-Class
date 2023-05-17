579ff7c73ebdbb69a946d1ae0197fef87df5786c
rsevial <rebekahsevial@gmail.com>
2023-05-17 13:49:54 +0800

    Pseudocode for Test Driver Program

--- TestTV.py
+++ TestTV.py
@@ -1,4 +1,9 @@
 # Programmed by: Rebekah Joy E. Sevial
 # Test Driver Program
 
+# Import the TV class of TV file
+# Define function for testing the two TV's
+# Assigning the values of each attributes of TV1 and TV2
+# Print the TV1's and TV2's channel and volume
+# Call the main function
 
