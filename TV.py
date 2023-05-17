f9ebb3126443052b25bb92d13262a61fd40eae9f
rsevial <rebekahsevial@gmail.com>
2023-05-17 09:29:33 +0800

    Define function to turn off the TV

--- TV.py
+++ TV.py
@@ -11,8 +11,10 @@ class TV:
     # Define function to turn on the TV
     def turnOn(self):
         self.power = True
-# Define function to turn off the TV
-# Define function to return the channel
+    # Define function to turn off the TV
+    def turnOff(self):
+        self.power = False
+    # Define function to return the channel
 # Define function to check if the channel is existing
 # Define function to return the volume
 # Define function to check if the volume is within the range
